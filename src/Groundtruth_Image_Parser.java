16ceb6eac36d2c6cc5619de74035c5dc589a7808
Eashwar Mohan <luigi629@gmail.com>
2017-02-01 20:42:01 -0500

    Many Changes
    gg

--- src/Groundtruth_Image_Parser.java
+++ src/Groundtruth_Image_Parser.java
@@ -1,16 +1,13 @@
 
 
-import edu.wpi.first.wpilibj.networktables.NetworkTable;
+//import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
 import java.applet.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.awt.geom.AffineTransform;
-import java.awt.image.*;
 import java.io.*;
 import java.util.Arrays;
-
-import javax.imageio.ImageIO;
+import java.math.*;
 
 
 public class Groundtruth_Image_Parser extends Applet implements ActionListener
@@ -22,41 +19,32 @@ public class Groundtruth_Image_Parser extends Applet implements ActionListener
 //	NetworkTable _bobby;
 	
 	String groundtruth_images;
-	byte[] left_image;
-	byte[] right_image;
+	char[] left_image_raw;
+	char[] right_image_raw;
 	
-	Image img_l = null;
-	Image img_r = null;
+	int[] left_image_pixels = null;
+	int[] right_image_pixels = null;
 	
+	int pixel_size = 15;
 	
 	public void init()
 	{
 		//initialize table
 //		_bobby = NetworkTable.getTable("SmartDashboard");
 		
-		Button b1 = new Button("Get Image");
+		Button b1 = new Button("Get Images");
 		add(b1);
 		b1.addActionListener(this);
 	}
 	
 	public void actionPerformed(ActionEvent e)
 	{
-		updateData();
-		img_l = getImage(getCodeBase(), "left_image");
-		img_r = getImage(getCodeBase(), "right_image");
-	}
-	
-/**
- * Calls all of the functions below.
- */
-	public void updateData()
-	{
 		getImageData();
 		splitImage();
-		left_image = reorderImage(left_image);
-		byteArrayToImage(left_image, "left_image");
-		right_image = reorderImage(right_image);
-		byteArrayToImage(right_image, "right_image");
+//		left_image_raw = reorderImage(left_image_raw);
+		left_image_pixels = charArrayToIntensity(left_image_raw);
+//		right_image_raw = reorderImage(right_image_raw);
+		right_image_pixels = charArrayToIntensity(right_image_raw);
 		repaint();
 	}
 	
@@ -65,9 +53,8 @@ public class Groundtruth_Image_Parser extends Applet implements ActionListener
  */
 	public void getImageData()
 	{
-		char[] charArray = new char[648];
-		Arrays.fill(charArray, 'A');
-		groundtruth_images = new String(charArray);//_bobby.getString("Groundtruth raw image", "");
+
+//		groundtruth_images = _bobby.getString("Groundtruth raw image", "");
 		
 	}
 	
@@ -76,8 +63,10 @@ public class Groundtruth_Image_Parser extends Applet implements ActionListener
  */
 	public void splitImage()
 	{
-		left_image = groundtruth_images.substring(0, 323).getBytes();
-		right_image = groundtruth_images.substring(324, 647).getBytes();
+		char[] testImages = new char[324];
+		Arrays.fill(testImages, (char) 20);
+		left_image_raw = testImages;//groundtruth_images.substring(0, 323).toCharArray();
+		right_image_raw = testImages;//groundtruth_images.substring(324, 647).toCharArray();
 	}
 	
 /**
@@ -85,72 +74,70 @@ public class Groundtruth_Image_Parser extends Applet implements ActionListener
  * @param raw_image: the image to be reordered
  * @return
  */
-	public byte[] reorderImage(byte[] raw_image)
+	public char[] reorderImage(char[] raw_image)
 	{				
-		byte[] ordered_image = new byte[324];
-
-			for (int j = 17; j < 0; j--)
+		char[] ordered_image = new char[324];
+		int counter = 0;
+		for (int i = 17; i < 0; i--)
+		{
+			for (int j = 0; j > 17; j++)
 			{
-				for (int k = 0; k > 17; k++)
-				{
-					ordered_image[k + ((j - 17) * -18)] = raw_image[((k * 18) + 1) + j];
-				}
+				ordered_image[counter] = raw_image[i + (j * 18)];
+				counter++;
 			}
+		}
 		return ordered_image;
 	}
+
 	
-/**
- * Converts an array of bytes to an image, and saves it in the codebase's directory.
- * @param image: the byte array containing the image data to be converted
- * @param imagename: a string to be used as the filename
- */
-	public void byteArrayToImage(byte[] image, String imagename)
+	public int[] charArrayToIntensity(char[] data)
 	{
-		try
+		int[] rgbdata = new int[data.length];
+		
+		for (int i = 0; i < data.length; i++)
 		{
-			InputStream in = new ByteArrayInputStream(image);
-			BufferedImage buffered_img = ImageIO.read(in);
-			buffered_img = scale(buffered_img, BufferedImage.TYPE_INT_ARGB, 90, 90, 5, 5);
-			ImageIO.write((RenderedImage)buffered_img, "jpg", new File(getCodeBase().toString() + imagename + Long.toString(System.currentTimeMillis())));
+			char pixel = data[i];
+			pixel = (char) (pixel & 63);
+			rgbdata[i] = ((int) pixel) * 255 / 63;
+
 		}
-		catch (IOException e) 
-		{
-			System.out.println("Image conversion failed");
-		}
+		
+		return rgbdata;
 	}
 
-/**
- * Scales a given bufferedimage.
- * thanks @user A4L on stackoverflow
- * 
- * @param sbi image to scale
- * @param imageType type of image
- * @param dWidth width of destination image
- * @param dHeight height of destination image
- * @param fWidth x-factor for transformation / scaling
- * @param fHeight y-factor for transformation / scaling
- * @return scaled image
- */
-	public static BufferedImage scale(BufferedImage sbi, int imageType, int dWidth, int dHeight, double fWidth, double fHeight) {
-	    BufferedImage dbi = null;
-	    if(sbi != null) {
-	        dbi = new BufferedImage(dWidth, dHeight, imageType);
-	        Graphics2D g = dbi.createGraphics();
-	        AffineTransform at = AffineTransform.getScaleInstance(fWidth, fHeight);
-	        g.drawRenderedImage(sbi, at);
-	    }
-	    return dbi;
-	}
 
 /**
  * Only paints the images if they are not null, and they are only not null after the button has been pressed once.
  */
 	public void paint(Graphics g)
 	{
-		if (img_l != null && img_r != null)
+		if (left_image_pixels != null)
 		{
-			g.drawImage(img_l, 0, 20, this);
-			g.drawImage(img_l, 100, 20, this);
+			int l_counter = 0;
+			for (int y = 0; y< 18; y++)
+			{
+				for (int x = 0; x < 18; x++)
+				{
+					g.setColor(new Color(left_image_pixels[l_counter], left_image_pixels[l_counter], left_image_pixels[l_counter]));
+					l_counter++;
+					g.fillRect(30 + (x * pixel_size), 50 + (y * pixel_size), pixel_size, pixel_size);
+				}
+	
+			}
+		}
+		if (right_image_pixels != null)
+		{
+			int r_counter = 0;
+			for (int y = 0; y< 18; y++)
+			{
+				for (int x = 0; x < 18; x++)
+				{
+					g.setColor(new Color(right_image_pixels[r_counter], right_image_pixels[r_counter], right_image_pixels[r_counter]));
+					r_counter++;
+					g.fillRect(350 + (x * pixel_size), 50 + (y * pixel_size), pixel_size, pixel_size);
+				}
+	
+			}
 		}
 	}
 }
